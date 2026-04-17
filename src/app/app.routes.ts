eb0721b5bd33f66d3cd91b4b8cc425924e7d246e
Mauri2498 <pxnda2498@gmail.com>
2026-04-17 00:23:04 -0400

    configuracion del header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Productos } from './componentes/productos/productos';
+import { Categorias } from './componentes/categorias/categorias';
+import { Clientes } from './componentes/clientes/clientes';
+
+export const routes: Routes = [
+  { path: 'productos', component: Productos },
+  { path: 'categorias', component: Categorias },
+  { path: 'clientes', component: Clientes },
+  { path: '', redirectTo: 'productos', pathMatch: 'full' }
+];
